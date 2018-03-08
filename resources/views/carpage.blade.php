2cb66c56369b4b7ba31fed5cfe0f34f9ea5f895a
richer0718 <1127129489@qq.com>
2018-03-08 13:28:17 +0800

    Update carpage.blade.php

--- resources/views/carpage.blade.php
+++ resources/views/carpage.blade.php
@@ -8,6 +8,8 @@
 <form action="{{ url('api/postCarUrl') }}" method="post">
    编号：<input type="text"  name="number" />
     验证码：<input type="text" name="code" />
+    <input type="hidder" name="time" value="{{$data['time']}}" />
+    <input type="hidder" name="file" value="{{$data['file']}}" />
     <img src="{{ $data['img'] }}" />
     <input type="submit" value="提交"/>
 </form>
