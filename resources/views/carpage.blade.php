a491394ee4d7effe47442179737d66d0ff2b3b2a
richer0718 <1127129489@qq.com>
2018-03-08 13:08:38 +0800

    Create carpage.blade.php

---
+++ resources/views/carpage.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body data-spy="scroll" data-offset="0" data-target="#navigation">
+<form action="{{ url('api/postCarUrl') }}" method="post">
+   编号：<input type="text"  name="number" />
+    验证码：<input type="text" name="code" />
+    <img src="{{ $data['img'] }}" />
+    <input type="submit" value="提交"/>
+</form>
+</body>
+</html>
